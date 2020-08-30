27f8c407c33dec17971ae1702f6d655bdd6c169a
Nadiantara <wayannadiantara@gmail.com>
2020-08-30 10:46:15 +0700

    disconnecting app.py from echoreview.py, echoreview.py has been deleted

--- app.py
+++ app.py
@@ -34,14 +34,32 @@ st.write("""
 ### Rating for each version
 """)
 
+#plotting apple rating
+def apple_plot():
+  apple_df=pd.read_csv("1031922175_GB_overall_28082020.csv")
+  pd.crosstab(apple_df['im:rating'], apple_df['im:version'], margins=True)
+  ct = pd.crosstab(apple_df['im:version'], apple_df['im:rating'])
+  plot = ct.tail(15).plot(kind="bar",figsize=(15, 3))
+  plot.set_xlabel("version")
+  plot.set_ylabel("rating")
+  return plot
+
+#plotting android rating
+def google_plot():
+  google_df=pd.read_csv("echo.co.uk_GB_overall_28082020.csv")
+  pd.crosstab(google_df['score'], google_df['reviewCreatedVersion'], margins=True)
+  ct = pd.crosstab(google_df['reviewCreatedVersion'], google_df['score'])
+  plot = ct.tail(15).plot(kind="bar",figsize=(15, 3))
+  plot.set_xlabel("version")
+  plot.set_ylabel("rating")
+  return plot
+
 # Display version plot for each OS
 def version_plot(selected_store):
   if selected_store == 'Apple':
-    from echoreview import apple_plot
     apple_plot()
     st.pyplot()
   else:
-    from echoreview import google_plot
     google_plot()
     st.pyplot()
 
@@ -71,7 +89,6 @@ def main_echo():
   # our default solver is 'liblinear'
   if selected_solver == 'liblinear':
     final_df = pd.read_pickle('final_df.pkl')
-    from echoreview import get_summed_scores
 
     #assign each sum result with their label into a dictionary
     sum_dict=get_summed_scores(final_df,df.columns[1:7].values.astype(str))
@@ -95,7 +112,6 @@ def main_echo():
     result_dict=dict(tuple([(features[i],coeffs[0][i]) for i in range(vocab_size)]))
     result_df=pd.DataFrame(sorted(result_dict.items(),key=lambda x:x[1]),columns=['word','coeff'])
     #find outlier
-    from echoreview import find_outliers
     lengths=combined_df_preprocessed['review'].apply(lambda x: len(x.split(" "))).values
     lower_outlier,upper_outlier=find_outliers(lengths)
     combined_df_preprocessed['z_score']=zscore(lengths)
@@ -107,13 +123,11 @@ def main_echo():
     combined_df_preprocessed['probabilities']=combined_df_preprocessed['z_score'].apply(lambda x:1-stats.norm.cdf(x))
 
     #take a look for the score and assign it to a new dataframe
-    from echoreview import lookup
     score=lookup(combined_df_preprocessed,result_dict)
     log_r_scored_df=combined_df_preprocessed.copy()
     log_r_scored_df['score']=score
 
     #scaling: divide outlier with its zscore
-    from echoreview import outlier_scaling
     log_r_scored_df_scaled=outlier_scaling(log_r_scored_df,upper_outlier,lower_outlier)
 
 
@@ -123,7 +137,6 @@ def main_echo():
     # fill null with zero
     final_df.fillna(0,inplace=True)
 
-    from echoreview import get_summed_scores
     #assign each sum result with their label into a dictionary
     sum_dict=get_summed_scores(final_df,df.columns[1:7].values.astype(str))
     #Sort the value 
@@ -151,6 +164,57 @@ def plot_dict(dict_value):
   return bar
 
 
+#######################################################################################
+#secondary function
+
+
+
+#get summed score from dictionary
+def get_summed_scores(df,col):
+  """ Inputs: 
+  df--dataframe to get sum of scores from. (should be scaled)
+  col--list of column names
+
+  Outputs:
+  Dictionary containing the mappings of the column names, and the severance score values """
+  sum_array=[]
+  for c in col:
+    sum=abs(np.sum(df[df[c]==1]['score'].values))
+    sum_array.append((c,sum))
+  sum_dict=dict(tuple(sum_array))
+  return sum_dict
+
+# get the outlier
+def find_outliers(array):
+  lower_outlier=int(np.quantile(array,0.25)-stats.iqr(array)*1.5)
+  upper_outlier=int(stats.iqr(array)*1.5+np.quantile(array,0.75))
+  return lower_outlier, upper_outlier
+
+#lookup and scoring  
+def lookup(df,ref):
+  val=df['review'].values
+  score_list=[]
+  for i in val:
+    score=0
+    for j in i.split(" "):
+      if j in ref:
+        score+=ref[j]
+      else:
+        score+=0
+  
+    score_list.append(score)
+  return score_list 
+
+#outlier scaling with zscore
+def outlier_scaling(df, upper, lower):
+  #Function assumes that lengths have been added to sentences
+  longg=df[df['lengths']>upper].index
+  df['score'].loc[longg]=df['score'].loc[longg]/abs(df['z_score'].loc[longg])
+  if lower>0:
+    #so if outlier is lets say 1.6, rounded to 1, we find and scale for those sentences with lengths <2
+    small=df[df['lengths']<lower+1].index
+    df['score'].loc[small]=df['score'].loc[small]*abs(df['z_score'].loc[small])
+  return df
 #######################################################################################
 if __name__=='__main__':
     main_echo()
